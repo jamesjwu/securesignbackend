3e7e8b62f463762a17be20f0f8b90d5ef6992b08
Anna Tan <rtan@andrew.cmu.edu>
2015-09-19 16:42:00 -0400

    type checking

--- app/python/train_models.py
+++ app/python/train_models.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from trainer import Predictor
+#from trainer import Predictor
 
 parser = ArgumentParser()
 parser.add_argument("--userid", type=str)
@@ -7,24 +7,25 @@ parser.add_argument("--imgurl", type=str)
 parser.add_argument("--train", action='store_true')
 parser.add_argument("--add", action='store_true')
 parser.add_argument("--is_pos", action='store_true')
-(options, args) = parser.parse_args()
-userid = options.userid
-imgurl = options.imgurl
-predictor = Predictor(userid)
+args = parser.parse_args()
+userid = args.userid
+imgurl = args.imgurl
+#predictor = Predictor(userid)
 
 pos_file = "../data/" + userid + ".pos"
 neg_file = "../data/all_data"
-# Assuming the pos file has been created by Javascript 
-if options.add:
+# Assuming the pos file has byeen created by Javascript 
+if args.add:
 	predictor.add_to_training_set(imgurl, options.is_pos)
-if options.train:
+elif args.train:
 	with open(pos_file, "r") as pos, open(neg_file, "r") as neg:
 		pos_data = pos.readlines()
 		neg_data = neg.readlines()
 	predictor.train(pos_data, neg_data)
 else:
-	print(predictor.predict(imgurl))
-	with open(pos_file, "a") as pos:
-		pos.write(imgurl + "\n")
+	prediction = predictor.predict(imgurl)
+	if prediction:
+		with open(pos_file, "a") as pos:
+			pos.write(imgurl + "\n")
 	with open(neg_file, "a") as neg:
 		neg.write(imgurl + "\n")
