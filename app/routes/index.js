49bc6477472800b326e3e10189c1eff8e0fa0594
James Wu <jameswu@cmu.edu>
2015-09-19 16:43:02 -0400

    Added code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,30 @@
-var express = require('express');
 'use strict';
+
+var express = require('express');
+
+var PythonShell = require('python-shell');
+
+var options = {
+  mode: 'text',
+  scriptPath: './python',
+};
+
 
 var router = express.Router();
 
 router.get('/', function (req, res) {
-	var id = req.body.personID;
-	var url = req.body.imageURL;
-	var isAuthenticated = test(id, url); 
-    res.send(isAuthenticated.toString());
+	PythonShell.run('trainer.py', options, function (err, results) {
+    if (err) console.log(err);
+      // results is an array consisting of messages collected during execution
+      console.log('results: %j', results);
+    });
+    res.render("index", {title:"asldijaslidj"});
 });
 
+
+router.post('/api/predict', function(req, res) {
+    var id = req.body.personID;
+    var url = req.body.imageURL;
+    
+});
 module.exports=router;
